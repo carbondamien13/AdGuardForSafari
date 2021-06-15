a8c6f83966b9ecca1ff21447594b8b01324cab3f
tvinzz <o.labudko@adguard.com>
2021-06-15 18:11:36 +0300

    fixed signs
    Former-commit-id: 3f256274e40a1b9fa6b2ed0d09c7707070c51f90

--- Scripts/sign-build.sh
+++ Scripts/sign-build.sh
@@ -22,44 +22,43 @@ if [[ ${AG_STANDALONE} == "true" ]]; then
 RT_OPTIONS=" -o runtime"
 fi
 
-SIGN_OPTIONS="--verbose=2 --force ${RT_OPTIONS} --timestamp --sign ${CODE_SIGN_IDENTITY}"
-ENTITLEMENTS_OPTIONS="--entitlements ${AG_ELECTRON_CHILD_ENT}"
+SIGN_OPTIONS="--verbose=2 --force$RT_OPTIONS --timestamp --entitlements $AG_ELECTRON_CHILD_ENT"
 
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "${RESOURCES}/libs/ConverterTool" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "${RESOURCES}/libs/ConverterTool" || exit 1
 
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "${RESOURCES}/app-x64.asar.unpacked/node_modules/safari-ext/build/Release/safari_ext_addon.node" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "${RESOURCES}/app-arm64.asar.unpacked/node_modules/safari-ext/build/Release/safari_ext_addon.node" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "${RESOURCES}/app-x64.asar.unpacked/node_modules/safari-ext/build/Release/safari_ext_addon.node" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "${RESOURCES}/app-arm64.asar.unpacked/node_modules/safari-ext/build/Release/safari_ext_addon.node" || exit 1
 
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libEGL.dylib" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libffmpeg.dylib" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libGLESv2.dylib" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libswiftshader_libEGL.dylib" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libswiftshader_libGLESv2.dylib" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Electron Framework.framework/Versions/A/Electron Framework" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Electron Framework.framework" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libEGL.dylib" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libffmpeg.dylib" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libGLESv2.dylib" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libswiftshader_libEGL.dylib" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libswiftshader_libGLESv2.dylib" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Electron Framework.framework/Versions/A/Libraries/libvk_swiftshader.dylib" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Electron Framework.framework/Versions/A/Electron Framework" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Electron Framework.framework" || exit 1
 
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/${PRODUCT_NAME} Helper.app" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/${PRODUCT_NAME} Helper (GPU).app" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/${PRODUCT_NAME} Helper (Plugin).app" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/${PRODUCT_NAME} Helper (Renderer).app" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/${PRODUCT_NAME} Helper.app" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/${PRODUCT_NAME} Helper (GPU).app" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/${PRODUCT_NAME} Helper (Plugin).app" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/${PRODUCT_NAME} Helper (Renderer).app" || exit 1
 
 if [[ ${AG_STANDALONE} == "true" ]]; then
-  codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Electron Framework.framework/Versions/A/Helpers/chrome_crashpad_handler" || exit 1
+  codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Electron Framework.framework/Versions/A/Helpers/chrome_crashpad_handler" || exit 1
 
-  codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/ReactiveObjC.framework/Versions/A/ReactiveObjC" || exit 1
-  codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/ReactiveObjC.framework" || exit 1
+  codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/ReactiveObjC.framework/Versions/A/ReactiveObjC" || exit 1
+  codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/ReactiveObjC.framework" || exit 1
 
-  codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Mantle.framework/Versions/A/Mantle" || exit 1
-  codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Mantle.framework" || exit 1
+  codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Mantle.framework/Versions/A/Mantle" || exit 1
+  codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Mantle.framework" || exit 1
 
-  codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Squirrel.framework/Versions/A/Resources/ShipIt" || exit 1
-  codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS "$FRAMEWORKS/Squirrel.framework" || exit 1
+  codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Squirrel.framework/Versions/A/Resources/ShipIt" || exit 1
+  codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "$FRAMEWORKS/Squirrel.framework" || exit 1
 fi
 
-ENTITLEMENTS_OPTIONS="--entitlements ${AG_ELECTRON_LOGINHELPER_ENT}"
+SIGN_OPTIONS="--verbose=2 --force$RT_OPTIONS --timestamp --entitlements $AG_ELECTRON_LOGINHELPER_ENT"
 
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS  "${LOGINS}/AdGuard Login Helper.app/Contents/MacOS/AdGuard Login Helper" || exit 1
-codesign $SIGN_OPTIONS $ENTITLEMENTS_OPTIONS  "${LOGINS}/AdGuard Login Helper.app" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "${LOGINS}/AdGuard Login Helper.app/Contents/MacOS/AdGuard Login Helper" || exit 1
+codesign $SIGN_OPTIONS --sign "${CODE_SIGN_IDENTITY}" "${LOGINS}/AdGuard Login Helper.app" || exit 1
 
 # DO NOT sign main app binary and bundle, because it will be signed by xCode on the final stage
-
